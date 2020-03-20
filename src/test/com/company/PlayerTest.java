d3bdf4188db9336a37a988e24ea4a0a1e0d3e8bf
tkhadir <tkhadir.dev@gmail.com>
2020-03-20 23:23:23 +0100

    minidb

---
+++ src/test/com/company/PlayerTest.java
@@ -0,0 +1,10 @@
+package test.com.company;
+
+import com.company.Player;
+
+public class PlayerTest {
+    public static void test() {
+        Player p = new Player();
+        System.out.println("test");
+    }
+}
